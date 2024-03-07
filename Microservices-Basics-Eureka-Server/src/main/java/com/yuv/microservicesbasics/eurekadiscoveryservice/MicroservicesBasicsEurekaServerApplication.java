5fe1523d628952970ac3e28e6f2758bf0b343ec5
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-03-07 18:58:37 +0530

    eureka server ready to run

--- Microservices-Basics-Eureka-Server/src/main/java/com/yuv/microservicesbasics/eurekadiscoveryservice/MicroservicesBasicsEurekaServerApplication.java
+++ Microservices-Basics-Eureka-Server/src/main/java/com/yuv/microservicesbasics/eurekadiscoveryservice/MicroservicesBasicsEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.yuv.microservicesbasics.eurekadiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroservicesBasicsEurekaServerApplication {
 
 	public static void main(String[] args) {
