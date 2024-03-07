69dd099bccb98df93909051eeaa9fc9c61b7f31e
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-03-07 17:08:39 +0530

    created Eureka Discovery Server

---
+++ Microservices-Basics-Eureka-Server/src/main/java/com/yuv/microservicesbasics/eurekadiscoveryservice/MicroservicesBasicsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.yuv.microservicesbasics.eurekadiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBasicsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesBasicsEurekaServerApplication.class, args);
+	}
+
+}
---
+++ Microservices-Basics-Eureka-Server/src/test/java/com/yuv/microservicesbasics/eurekadiscoveryservice/MicroservicesBasicsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuv.microservicesbasics.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBasicsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
