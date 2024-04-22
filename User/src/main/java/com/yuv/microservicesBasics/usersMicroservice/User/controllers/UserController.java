abd94807666ddce9b3e6a6f32cf00af369575f9f
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-04-23 01:26:17 +0530

    inbuilt load balancer in api gateway

--- User/src/main/java/com/yuv/microservicesBasics/usersMicroservice/User/controllers/UserController.java
+++ User/src/main/java/com/yuv/microservicesBasics/usersMicroservice/User/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.yuv.microservicesBasics.usersMicroservice.User.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+	@Autowired
+	Environment env;
+	
 	@GetMapping("/status/check")
 	public String status()
 	{
-		return "Hello";
+		return "Hello "+env.getProperty("local.server.port");
 	}
 	
 }
