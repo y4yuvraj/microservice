a8e5b0f2291bcf1d41ac848e51aeb8a3d3a3342a
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-04-22 19:31:30 +0530

    added spring cloud api gateway

---
+++ apiGateway/src/test/java/com/yuv/microserviceBasics/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuv.microserviceBasics.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
